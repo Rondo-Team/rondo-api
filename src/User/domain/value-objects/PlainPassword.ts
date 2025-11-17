523bf3b048997b27b958eed2a671b71d5a2cd479
Guillermo Díaz <alu0101505688@ull.edu.es>
2025-11-17 14:57:14 +0000

    feat: hashing passwords

---
+++ src/User/domain/value-objects/PlainPassword.ts
@@ -0,0 +1,20 @@
+import { PasswordIsInvalidError } from "../errors/PasswordIsInvalidError";
+
+export class PlainPassword {
+  value: string;
+
+  constructor(value: string) {
+    this.value = value;
+    this.ensureIsValid();
+  }
+
+  private ensureIsValid() {
+    const regex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+={}[\]|\\:;"'<>,.?/~`]).{8,}$/;
+    if (!regex.test(this.value)) throw new PasswordIsInvalidError();
+  }
+
+  toPrimitives() {
+    return this.value
+  }
+}
---
+++ src/User/infrastructure/security/BcryptPasswordHasher.ts
@@ -0,0 +1,15 @@
+import { PasswordHasherRepository } from "../../domain/repositories/PasswordHasherRepository";
+import bcrypt from "bcrypt"
+
+const SALT = process.env.HASH_SALT ?? 10
+
+
+export class BcryptPasswordHasher implements PasswordHasherRepository {
+  async hash(plain: string) {
+    return bcrypt.hash(plain, SALT);
+  }
+
+  async compare(plain: string, hashed: string) {
+    return bcrypt.compare(plain, hashed);
+  }
+}
