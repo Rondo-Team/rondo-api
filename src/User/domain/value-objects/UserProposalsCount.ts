77efe97903275e1ac5685f85faad274679aa037a
Guillermo Díaz <alu0101505688@ull.edu.es>
2025-11-17 12:48:41 +0000

    feat: added value object

---
+++ src/User/domain/value-objects/UserProposalsCount.ts
@@ -0,0 +1,20 @@
+import { ProposalsCountInvalidError } from "../errors/ProposalsCountInvalidError";
+
+export const UPPER_PROPOSALS_LIMIT = 10^5
+
+export class UserProposalsCount {
+  value: number;
+
+  constructor(value: number) {
+    this.value = value;
+  }
+
+  private ensureIsValid() {
+      if (
+        !Number.isInteger(this.value) ||
+        this.value < 0 ||
+        this.value > UPPER_PROPOSALS_LIMIT
+      )
+        throw new ProposalsCountInvalidError(this.value);
+    }
+}
