a97c82d031d0964f287d3cb9205aa2d803219fb7
Guillermo Díaz <alu0101505688@ull.edu.es>
2025-11-17 14:58:35 +0000

    feat: added value object to represent a hashed password

--- src/User/domain/value-objects/HashedPassword.ts
+++ src/User/domain/value-objects/HashedPassword.ts
@@ -1,16 +1,7 @@
-import { PasswordIsInvalidError } from "../errors/PasswordIsInvalidError";
-
 export class HashedPassword {
   value: string;
 
   constructor(value: string) {
     this.value = value;
-    this.ensureIsValid();
-  }
-
-  private ensureIsValid() {
-    const regex =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+={}[\]|\\:;"'<>,.?/~`]).{8,}$/;
-    if (!regex.test(this.value)) throw new PasswordIsInvalidError();
   }
 }
