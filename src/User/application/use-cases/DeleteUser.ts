f21fc8ca8043f15cdb0c239e869f6665ee19a2d8
Guillermo Díaz <alu0101505688@ull.edu.es>
2025-11-18 15:24:48 +0000

    fix: update imports

--- src/User/application/use-cases/DeleteUser.ts
+++ src/User/application/use-cases/DeleteUser.ts
@@ -1,13 +1,9 @@
 import { UserNotFoundByIdError } from "../../domain/errors/UserNotFoundByIdError";
 import { UserRepository } from "../../domain/repositories/UserRepository";
-import { UserFinder } from "../../domain/services/UserFinder";
 import { UserId } from "../../domain/value-objects/UserId";
 
 export class DeleteUser {
-  private readonly userFinder: UserFinder;
-  constructor(private userRepository: UserRepository) {
-    this.userFinder = new UserFinder(userRepository);
-  }
+  constructor(private userRepository: UserRepository) {}
 
   async run(id: string): Promise<void> {
     if (!(await this.userRepository.existsWithId(new UserId(id))))
