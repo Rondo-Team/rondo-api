51793b0fba2f93a3208436c0cd4d6cd59a11e227
Guillermo Díaz <alu0101505688@ull.edu.es>
2025-11-20 09:48:35 +0000

    fix: using global consts

--- src/User/domain/value-objects/UserName.ts
+++ src/User/domain/value-objects/UserName.ts
@@ -1,3 +1,4 @@
+import { USER_NAME_CHAR_LOWER_LIMIT, USER_NAME_CHAR_UPPER_LIMIT } from "@/config";
 import { UserNameContainsInvalidCharactersError } from "@/user/domain/errors/UserNameContainsInvalidCharactersError";
 import { UserNameIsTooLongError } from "@/user/domain/errors/UserNameIsTooLongError";
 import { UserNameIsTooShortError } from "@/user/domain/errors/UserNameIsTooShortError";
@@ -14,8 +15,8 @@ export class UserName {
     const nameLength = this.value.length;
     const regex = /^[a-zA-ZÀ-ÿ\s'-]+$/;
 
-    if (nameLength > 50) throw new UserNameIsTooLongError(this.value);
-    if (nameLength < 2) throw new UserNameIsTooShortError(this.value);
+    if (nameLength > USER_NAME_CHAR_UPPER_LIMIT) throw new UserNameIsTooLongError(this.value);
+    if (nameLength < USER_NAME_CHAR_LOWER_LIMIT) throw new UserNameIsTooShortError(this.value);
     if (!regex.test(this.value))
       throw new UserNameContainsInvalidCharactersError(this.value);
   }
