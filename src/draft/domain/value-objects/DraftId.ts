00f5a653c80203ca4a5f325289b86fc271bf2b78
Guillermo Díaz <alu0101505688@ull.edu.es>
2025-11-21 09:39:00 +0000

    feat: created abstract id class to represent Ids

--- src/draft/domain/value-objects/DraftId.ts
+++ src/draft/domain/value-objects/DraftId.ts
@@ -1,6 +1,6 @@
 import { Id } from "@/shared/domain/value-objects/Id";
 
-export class UserId extends Id {
+export class DraftId extends Id {
   constructor(value: string) {
     super(value);
   }
--- src/post/domain/value-objects/PostId.ts
+++ src/post/domain/value-objects/PostId.ts
@@ -1,20 +1,7 @@
-import { IdIsNotValidError } from "@/shared/domain/errors/IdIsNotValidError";
+import { Id } from "@/shared/domain/value-objects/Id";
 
-export class PostId {
-  value: string;
-
+export class PostId extends Id {
   constructor(value: string) {
-    this.value = value;
-    this.ensureIsValid();
-  }
-
-  private ensureIsValid() {
-    const regex =
-      /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-    if (!regex.test(this.value)) throw new IdIsNotValidError(this.value);
-  }
-
-  toPrimitives() {
-    return this.value;
+    super(value);
   }
 }
