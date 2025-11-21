aa33f5411d185a72d4df71b9e44d385e9e1563b7
Guillermo Díaz <alu0101505688@ull.edu.es>
2025-11-21 13:33:45 +0000

    fix: delete unnecesary imports

--- src/test/comment/Comment.test.ts
+++ src/test/comment/Comment.test.ts
@@ -1,21 +1,10 @@
 import { beforeEach, describe, expect, it } from "vitest";
 
-import { HashedPassword } from "@/shared/password-hashing/domain/value-objects/HashedPassword";
-import { comment } from "@/comment/domain/comment";
-import { commentCommentsCount } from "@/comment/domain/value-objects/commentCommentsCount";
-import { CreatedAt } from "@/shared/domain/value-objects/CreatedAt";
-import { commentEmail } from "@/comment/domain/value-objects/commentEmail";
-import { commentFavouritePostsCount } from "@/comment/domain/value-objects/commentFavouritePostsCount";
-import { commentId } from "@/comment/domain/value-objects/commentId";
-import { commentName } from "@/comment/domain/value-objects/commentName";
-import { commentPostsCount } from "@/comment/domain/value-objects/commentPostsCount";
-import { commentProfilePicture } from "@/comment/domain/value-objects/commentProfilePicture";
-import { commentProposalsCount } from "@/comment/domain/value-objects/commentProposalsCount";
-import { commentname } from "@/comment/domain/value-objects/commentname";
 import { Comment } from "@/comment/domain/Comment";
+import { CommentId } from "@/comment/domain/value-objects/CommentId";
 import { CommentMessage } from "@/comment/domain/value-objects/CommentMessage";
 import { PostId } from "@/post/domain/value-objects/PostId";
-import { CommentId } from "@/comment/domain/value-objects/CommentId";
+import { CreatedAt } from "@/shared/domain/value-objects/CreatedAt";
 import { UserId } from "@/user/domain/value-objects/UserId";
 
 describe("Comment model tests", () => {
