b0aba26816f55870b9bd62c3da32addf4e3539f9
Guillermo Díaz <gdiazbricio@gmail.com>
2025-11-24 19:20:10 +0000

    fix: fixed bugs in use cases related to user finder service

--- src/user/application/use-cases/ChangeUsername.ts
+++ src/user/application/use-cases/ChangeUsername.ts
@@ -2,6 +2,7 @@ import { UsernameAndNewUsernameAreEqualError } from "@/user/domain/errors/Userna
 import { UserRepository } from "@/user/domain/repositories/UserRepository";
 import { UserFinder } from "@/user/domain/services/UserFinder";
 import { UserUniquenessChecker } from "@/user/domain/services/UserUniquenessChecker";
+import { UserId } from "@/user/domain/value-objects/UserId";
 import { UserUsername } from "@/user/domain/value-objects/UserUsername";
 
 export class ChangeEmail {
@@ -13,12 +14,12 @@ export class ChangeEmail {
   }
 
   async run(id: string, newUsername: string): Promise<void> {
-    const user = await this.userFinder.findById(id);
+    const user = await this.userFinder.findById(new UserId(id));
 
     if (user.username === new UserUsername(newUsername))
       throw new UsernameAndNewUsernameAreEqualError(newUsername);
 
-    await this.userUniquenessChecker.ensureUsernameIsNotUsed(newUsername);
+    await this.userUniquenessChecker.ensureUsernameIsNotUsed(new UserUsername(newUsername));
     await user.changeUsername(new UserUsername(newUsername));
 
     return this.userRepository.edit(user);
