223da2cc98cbfe372406a60effe25660f3abe68b
Guillermo Díaz <alu0101505688@ull.edu.es>
2025-11-24 12:35:35 +0000

    test: added testing for comment favourites count value object

--- src/test/comment/Comment.test.ts
+++ src/test/comment/Comment.test.ts
@@ -6,6 +6,7 @@ import { CommentMessage } from "@/comment/domain/value-objects/CommentMessage";
 import { PostId } from "@/post/domain/value-objects/PostId";
 import { CreatedAt } from "@/shared/domain/value-objects/CreatedAt";
 import { UserId } from "@/user/domain/value-objects/UserId";
+import { CommentFavouritesCount } from "@/comment/domain/value-objects/CommentFavouritesCount";
 
 describe("Comment model tests", () => {
   let comment: Comment;
@@ -16,6 +17,7 @@ describe("Comment model tests", () => {
       new UserId("550e8400-e29b-41d4-a716-446655440000"),
       new PostId("550e8400-e29b-41d4-a716-446655440000"),
       new CommentMessage("ExampleMessage"),
+      new CommentFavouritesCount(1),
       new CreatedAt(new Date("2020-01-01"))
     );
 
@@ -28,6 +30,7 @@ describe("Comment model tests", () => {
     expect(comment.userId.value).toBe("550e8400-e29b-41d4-a716-446655440000");
     expect(comment.postId.value).toBe("550e8400-e29b-41d4-a716-446655440000");
     expect(comment.message.value).toBe("ExampleMessage");
+    expect(comment.favouritesCount.value).toBe(1)
     expect(comment.createdAt.value).toEqual(new Date("2020-01-01"));
   });
 });
---
+++ src/test/comment/value-objects/CommentFavouritesCount.test.ts
@@ -0,0 +1,29 @@
+import { CommentFavouritesCountIsInvalidError } from "@/comment/domain/errors/CommentFavouritesCountIsInvalidError";
+import { CommentFavouritesCount } from "@/comment/domain/value-objects/CommentFavouritesCount";
+import { COMMENT_FAVOURITES_UPPER_LIMIT } from "@/config";
+import { PostCommentsCountIsInvalidError } from "@/post/domain/errors/PostCommentsCountIsInvalidError";
+import { describe, expect, it } from "vitest";
+
+describe("Comment favourites count tests", () => {
+  it("does not fail if count is valid", () => {
+    expect(() => new CommentFavouritesCount(7)).not.toThrow();
+  });
+
+  it("throws an error if count is invalid (non integer number)", () => {
+    expect(() => new CommentFavouritesCount(7.7)).toThrowError(
+      PostCommentsCountIsInvalidError
+    );
+  });
+
+  it("throws an error if count is invalid (negative number)", () => {
+    expect(() => new CommentFavouritesCount(-7)).toThrowError(
+      PostCommentsCountIsInvalidError
+    );
+  });
+
+  it("throws an error if count is invalid (greateer than max)", () => {
+    expect(
+      () => new CommentFavouritesCount(COMMENT_FAVOURITES_UPPER_LIMIT + 1)
+    ).toThrowError(CommentFavouritesCountIsInvalidError);
+  });
+});
