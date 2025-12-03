29795631969a14bfdd2f18a09e4d94c267647a11
Guillermo Díaz <alu0101505688@ull.edu.es>
2025-12-03 14:16:09 +0000

    feat: added contenerizing in main

--- src/container.ts
+++ src/container.ts
@@ -1,6 +1,7 @@
 import { Container } from "inversify";
 import { MongoCommentRepository } from "./comment/infrastructure/repositories/MongoCommentRepository.ts";
 import { Token } from "./config/domain/Token.ts";
+import { createExpress } from "./shared/controllers/infrastructure/CreateExpress.ts";
 import { errorMiddleware } from "./shared/controllers/infrastructure/middlewares/ErrorMiddleware.ts";
 import { MongoModule } from "./shared/persistance/infrastructure/mongo/CreateMongoClient.ts";
 import { BcryptPasswordHasher } from "./shared/services/bcrypt/infrastructure/BcryptPasswordHasher.ts";
@@ -11,14 +12,13 @@ export const container = new Container();
 container.load(MongoModule);
 
 // Repos
-container
-  .bind(Token.COMMENT_REPOSITORY)
-  .toDynamicValue(MongoCommentRepository.create);
+container.bind(Token.COMMENT_REPOSITORY).toDynamicValue(MongoCommentRepository.create);
 
 // Services
-container
-  .bind(Token.PASSWORD_HASHER)
-  .toDynamicValue(BcryptPasswordHasher.create);
+container.bind(Token.PASSWORD_HASHER).toDynamicValue(BcryptPasswordHasher.create);
+
+// App
+container.bind(Token.APP).toDynamicValue(createExpress);
 
 // Middlewares
 container.bind(Token.ERROR_MIDDLEWARE).toConstantValue(errorMiddleware);
