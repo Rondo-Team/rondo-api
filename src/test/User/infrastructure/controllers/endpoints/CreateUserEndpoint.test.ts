fd9fd61b020c4253ceea1db85e141ec50d7d94f5
Guillermo Díaz <alu0101505688@ull.edu.es>
2025-12-10 12:44:26 +0000

    test: tested create user endpoint

--- src/test/User/infrastructure/controllers/endpoints/CreateUserEndpoint.test.ts
+++ src/test/User/infrastructure/controllers/endpoints/CreateUserEndpoint.test.ts
@@ -1,18 +1,21 @@
 import supertest from "supertest";
-import { beforeAll, describe, expect, it } from "vitest";
+import { beforeAll, beforeEach, describe, expect, it } from "vitest";
 import { Token } from "../../../../../config/domain/Token.ts";
 import { container } from "../../../../../container.ts";
-import { clearTestDatabase } from "../../../../utils/ClearTestDatabase.ts";
+import { clearTestDatabase } from "../../../../utils/clearTestDatabase.ts";
 
 let app;
 
 beforeAll(async () => {
-  await clearTestDatabase();
   app = await container.getAsync(Token.APP);
 });
 
+beforeEach(async () => {
+  await clearTestDatabase();
+});
+
 describe("create user endpoint tests", () => {
-  it("should create a user", async () => {
+  it("should create a user succesfully", async () => {
     const res = await supertest(app).post("/api/v1/users").send({
       id: "123e4567-e89b-12d3-a456-426614174000",
       email: "example@gmail.com",
@@ -21,7 +24,72 @@ describe("create user endpoint tests", () => {
       password: "PasswordExample10_",
     });
 
-    console.log(res.body);
     expect(res.status).toBe(201);
+  });
+
+  it("should not create user with an existent id", async () => {
+    // Insert first element
+    await supertest(app).post("/api/v1/users").send({
+      id: "123e4567-e89b-12d3-a456-426614174000",
+      email: "example@gmail.com",
+      username: "example",
+      name: "Example",
+      password: "PasswordExample10_",
+    });
+
+    // Insert second element
+    const res = await supertest(app).post("/api/v1/users").send({
+      id: "123e4567-e89b-12d3-a456-426614174000",
+      email: "example@gmail.com",
+      username: "example",
+      name: "Example",
+      password: "PasswordExample10_",
+    });
+
+    expect(res.status).toBe(409);
+  });
+
+  it("should not create user with an existent email", async () => {
+    // Insert first element
+    await supertest(app).post("/api/v1/users").send({
+      id: "123e4567-e89b-12d3-a456-426614174000",
+      email: "example@gmail.com",
+      username: "example",
+      name: "Example",
+      password: "PasswordExample10_",
+    });
+
+    // Insert second element
+    const res = await supertest(app).post("/api/v1/users").send({
+      id: "123e4567-e89b-12d3-a456-426614174001",
+      email: "example@gmail.com",
+      username: "anotherexample",
+      name: "Another Example",
+      password: "PasswordExample10_",
+    });
+
+    expect(res.status).toBe(409);
+  });
+
+  it("should not create user with an existent username", async () => {
+    // Insert first element
+    await supertest(app).post("/api/v1/users").send({
+      id: "123e4567-e89b-12d3-a456-426614174000",
+      email: "example@gmail.com",
+      username: "example",
+      name: "Example",
+      password: "PasswordExample10_",
+    });
+
+    // Insert second element
+    const res = await supertest(app).post("/api/v1/users").send({
+      id: "123e4567-e89b-12d3-a456-426614174001",
+      email: "anotherexample@gmail.com",
+      username: "example",
+      name: "Example",
+      password: "PasswordExample10_",
+    });
+
+    expect(res.status).toBe(409);
   });
 });
