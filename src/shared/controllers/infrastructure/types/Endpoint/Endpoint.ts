27c1c6874d7749e855e7b02b6d6faa118455fddb
Guillermo Díaz <gdiazbricio@gmail.com>
2025-12-21 17:56:38 +0000

    fix: update endpoint type

--- src/shared/controllers/infrastructure/types/Endpoint/Endpoint.ts
+++ src/shared/controllers/infrastructure/types/Endpoint/Endpoint.ts
@@ -1,10 +1,6 @@
-import type { RequestHandler } from "express";
-import type { EndpointDocs } from "./EndpointDocs.ts";
-
 export type Endpoint = {
   method: "put" | "post" | "patch" | "delete" | "get";
   path: string;
-  handlers: Array<RequestHandler>;
+  handlers: Array<any>;
   secured: boolean;
-  docs?: EndpointDocs;
 };
