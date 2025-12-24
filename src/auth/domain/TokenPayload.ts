4e3f92116faf7483c6a830461fd2a8ac23e25bf6
Guillermo Díaz <alu0101505688@ull.edu.es>
2025-12-24 12:21:37 +0000

    fix: change token payload fields in order to match standard

--- src/auth/domain/TokenPayload.ts
+++ src/auth/domain/TokenPayload.ts
@@ -2,9 +2,9 @@ import { Role } from "./value-objects/Role.ts";
 import { TokenPurpose } from "./value-objects/TokenPurpose.ts";
 
 export type TokenPayload = {
-  userId: string;
-  createdAt: number;
-  expireDate: number;
+  sub: string; // User id
+  iat: number; // Created At
+  exp: number; // Expire Time
   role: Role;
   purpose: TokenPurpose;
 };
