aa1e3599ebe6442a8acc8a4961c0693a00f10dab
Guillermo Díaz <alu0101505688@ull.edu.es>
2025-12-24 12:26:14 +0000

    fix: discard changes

--- src/shared/controllers/infrastructure/CreateHono.ts
+++ src/shared/controllers/infrastructure/CreateHono.ts
@@ -3,8 +3,8 @@ import { jwt } from "hono/jwt";
 import type { ResolutionContext } from "inversify";
 import { Token } from "../../../config/domain/Token.ts";
 import { config } from "../../../config/infrastructure/config.ts";
-import { errorMiddleware } from "./middlewares/ErrorMiddleware.ts";
 import type { Endpoint } from "./types/Endpoint.ts";
+import { errorMiddleware } from "./middlewares/ErrorMiddleware.ts";
 
 export async function createHono(container: ResolutionContext) {
   const app = new Hono();
@@ -12,15 +12,18 @@ export async function createHono(container: ResolutionContext) {
 
   endpoints.forEach((endpoint) => {
     const isSecured = endpoint.secured;
-    if (isSecured)
-      app.use(
-        endpoint.path,
-        jwt({
-          secret: config.jwt.secret,
-          cookie: "accessToken",
-        })
-      );
-    app.on(endpoint.method, [endpoint.path], ...endpoint.handlers);
+    const handlers = [
+      ...(isSecured
+        ? [
+            jwt({
+              secret: config.jwt.secret,
+              cookie: "accessToken",
+            }),
+          ]
+        : []),
+      ...endpoint.handlers,
+    ];
+    app.on(endpoint.method, [endpoint.path], ...handlers);
   });
 
   app.onError(errorMiddleware);
