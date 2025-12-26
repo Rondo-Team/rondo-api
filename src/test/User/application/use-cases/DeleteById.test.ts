5fdedbbd83f31dbe71021f599b48ad0568a33667
Guillermo Díaz <alu0101505688@ull.edu.es>
2025-12-26 11:02:14 +0000

    test: add tests for user deletion

---
+++ src/test/User/application/use-cases/DeleteById.test.ts
@@ -0,0 +1,34 @@
+import { describe, expect, it, vi } from "vitest";
+import {
+  MANOLO_LOPEZ,
+  PEDRO_MARTINEZ,
+} from "../../../../shared/utils/domain/fixtures/users.ts";
+import { DeleteUserById } from "../../../../user/application/use-cases/DeleteUserById.ts";
+import { UnauthorizedUserActionError } from "../../../../user/domain/errors/UnauthorizedUserActionError.ts";
+
+describe("Delete user by id use case tests", () => {
+  const userRepo = {
+    create: vi.fn(),
+    getOneById: vi.fn(),
+    getOneByEmail: vi.fn(),
+    existsWithId: vi.fn().mockResolvedValue(true),
+    existsWithEmail: vi.fn(),
+    existsWithUsername: vi.fn(),
+    edit: vi.fn(),
+    deleteById: vi.fn(),
+  };
+
+  const deleteUserById = new DeleteUserById(userRepo);
+
+  it("Should delete a user succesfully", async () => {
+    await deleteUserById.run(MANOLO_LOPEZ.id, MANOLO_LOPEZ.id);
+
+    expect(userRepo.deleteById).toBeCalledTimes(1);
+  });
+
+  it("Should not delete a user if actor is not the user to delete", async () => {
+    expect(
+      async () => await deleteUserById.run(MANOLO_LOPEZ.id, PEDRO_MARTINEZ.id)
+    ).rejects.toThrow(UnauthorizedUserActionError);
+  });
+});
