99c2af78868794be572869e760cc8e8c12dc83ff
Guillermo Díaz <alu0101505688@ull.edu.es>
2025-12-26 09:45:57 +0000

    feat: add new user fixture

--- src/shared/utils/domain/fixtures/users.ts
+++ src/shared/utils/domain/fixtures/users.ts
@@ -9,5 +9,19 @@ export const MANOLO_LOPEZ = {
   proposalsCount: 0,
   favouritePostsCount: 0,
   commentsCount: 0,
-  createdAt: new Date()
-}
+  createdAt: new Date(),
+};
+
+export const PEDRO_MARTINEZ = {
+  id: "123e4567-e89b-12d3-a456-426614174000",
+  email: "pedromartinez@gmail.com",
+  username: "pedrobestie",
+  name: "Pedro Martínez",
+  profilePicture: "https://example-picture-2.com",
+  password: "PedritoVibes12_",
+  postsCount: 0,
+  proposalsCount: 0,
+  favouritePostsCount: 0,
+  commentsCount: 0,
+  createdAt: new Date(),
+};
