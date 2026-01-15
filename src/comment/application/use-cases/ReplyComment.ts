7db559e5c23a4802b252f1cd021f146c3eef4d72
Guillermo <alu0101505688@ull.edu.es>
2026-01-15 15:31:55 +0000

    feat: add validation for comment replies to ensure they refer to the same post

--- src/comment/application/use-cases/ReplyComment.ts
+++ src/comment/application/use-cases/ReplyComment.ts
@@ -7,8 +7,10 @@ import { UserFinder } from "../../../user/domain/services/UserFinder.ts";
 import { UserId } from "../../../user/domain/value-objects/UserId.ts";
 import { Comment } from "../../domain/Comment.ts";
 import { CommentNotFoundByIdError } from "../../domain/errors/CommentNotFoundByIdError.ts";
+import { CommentReplyRefersToDifferentPostError } from "../../domain/errors/CommentReplyRefersToDifferentPostError.ts";
 import { CommentWithIdAlreadyExistsError } from "../../domain/errors/CommentWithIdAlreadyExistsError.ts";
 import type { CommentRepository } from "../../domain/repositories/CommentRepository.ts";
+import { CommentFinder } from "../../domain/services/CommentFinder.ts";
 import { CommentFavouritesCount } from "../../domain/value-objects/CommentFavouritesCount.ts";
 import { CommentId } from "../../domain/value-objects/CommentId.ts";
 import { CommentMessage } from "../../domain/value-objects/CommentMessage.ts";
@@ -19,6 +21,7 @@ export class ReplyComment {
   private userRepository: UserRepository;
   private userFinder: UserFinder;
   private postFinder: PostFinder;
+  private commentFinder: CommentFinder;
   constructor(
     commentRepository: CommentRepository,
     postRepository: PostRepository,
@@ -29,6 +32,7 @@ export class ReplyComment {
     this.userRepository = userRepository;
     this.userFinder = new UserFinder(userRepository);
     this.postFinder = new PostFinder(postRepository);
+    this.commentFinder = new CommentFinder(commentRepository);
   }
 
   async run(
@@ -60,6 +64,13 @@ export class ReplyComment {
     // Ensure comment with parentId id exists
     if (!(await this.commentRepository.existsWithId(new CommentId(parentId))))
       throw new CommentNotFoundByIdError(id);
+    // Ensure parentId is referring to a comment in the same post
+    const parentComment = await this.commentFinder.findById(
+      CommentId.fromPrimitives(parentId)
+    );
+    if (parentComment.postId.toPrimitives() !== postId)
+      throw new CommentReplyRefersToDifferentPostError(id, postId);
+
     // Trigger a post CommentCount update and a user commentsCount update.
     user.addComment();
     post.addComment();
