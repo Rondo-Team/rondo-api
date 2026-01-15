32a95775bd9da37e53c7151648a08fae1b850c51
Guillermo <alu0101505688@ull.edu.es>
2026-01-15 15:48:25 +0000

    feat: add ReplyComment use case and corresponding integration tests

---
+++ src/test/comment/infrastructure/ReplyCommentEndpoint.integration.test.ts
@@ -0,0 +1,104 @@
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from "vitest";
+import { Token } from "../../../config/domain/Token.ts";
+import { container } from "../../../container.ts";
+import {
+  SAMPLE_CHILD_COMMENT,
+  SAMPLE_PARENT_COMMENT,
+} from "../../../shared/utils/domain/fixtures/comments.ts";
+import { ONE_STEP_POST } from "../../../shared/utils/domain/fixtures/posts.ts";
+import { MANOLO_LOPEZ } from "../../../shared/utils/domain/fixtures/users.ts";
+import { clearTestDatabase } from "../../utils/clearTestDatabase.ts";
+import { insertComment } from "../../utils/insertComment.ts";
+import { insertPost } from "../../utils/insertPost.ts";
+import { loginUser, registerUser } from "../../utils/userAuthentication.ts";
+
+let app;
+
+beforeAll(async () => {
+  app = await container.getAsync(Token.APP);
+});
+
+beforeEach(async () => {
+  await clearTestDatabase();
+});
+
+afterAll(async () => {
+  await clearTestDatabase();
+});
+
+describe("reply comment endpoint tests", () => {
+  it("should reply to a comment successfully", async () => {
+    await registerUser(MANOLO_LOPEZ);
+    const accessToken = await loginUser(MANOLO_LOPEZ);
+    await insertPost(ONE_STEP_POST);
+    await insertComment(SAMPLE_PARENT_COMMENT);
+
+    const res = await app.request(
+      `/api/v1/comment/reply/${SAMPLE_CHILD_COMMENT.parentId}`,
+      {
+        method: "POST",
+        headers: {
+          Cookie: `accessToken=${accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: SAMPLE_CHILD_COMMENT.id,
+          postId: SAMPLE_CHILD_COMMENT.postId,
+          parentCommentId: SAMPLE_CHILD_COMMENT.parentId,
+          message: SAMPLE_CHILD_COMMENT.message,
+        }),
+      }
+    );
+
+    expect(res.status).toBe(201);
+  });
+
+  it("should not reply to a comment if parent comment does not exist", async () => {
+    await registerUser(MANOLO_LOPEZ);
+    const accessToken = await loginUser(MANOLO_LOPEZ);
+    await insertPost(ONE_STEP_POST);
+
+    const res = await app.request(
+      `/api/v1/comment/reply/${SAMPLE_CHILD_COMMENT.parentId}`,
+      {
+        method: "POST",
+        headers: {
+          Cookie: `accessToken=${accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: SAMPLE_CHILD_COMMENT.id,
+          postId: SAMPLE_CHILD_COMMENT.postId,
+          parentCommentId: SAMPLE_CHILD_COMMENT.parentId,
+          message: SAMPLE_CHILD_COMMENT.message,
+        }),
+      }
+    );
+
+    expect(res.status).toBe(404);
+  });
+
+  it("should not reply to a comment if post does not exist", async () => {
+    await registerUser(MANOLO_LOPEZ);
+    const accessToken = await loginUser(MANOLO_LOPEZ);
+
+    const res = await app.request(
+      `/api/v1/comment/reply/${SAMPLE_CHILD_COMMENT.parentId}`,
+      {
+        method: "POST",
+        headers: {
+          Cookie: `accessToken=${accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: SAMPLE_CHILD_COMMENT.id,
+          postId: SAMPLE_CHILD_COMMENT.postId,
+          parentCommentId: SAMPLE_CHILD_COMMENT.parentId,
+          message: SAMPLE_CHILD_COMMENT.message,
+        }),
+      }
+    );
+
+    expect(res.status).toBe(404);
+  });
+});
