f77a4dfd918d1dc2d5ea9c90bafd9b4e153053c1
Guillermo <alu0101505688@ull.edu.es>
2026-01-15 12:53:52 +0000

    feat: add permission check to DeleteCommentById use case

--- src/comment/application/use-cases/DeleteCommentById.ts
+++ src/comment/application/use-cases/DeleteCommentById.ts
@@ -1,7 +1,9 @@
 import type { PostRepository } from "../../../post/domain/repositories/PostRepository.ts";
 import { PostFinder } from "../../../post/domain/services/PostFinder.ts";
+import { ResourceAccessChecker } from "../../../shared/domain/services/ResourceAccessChecker.ts";
 import type { UserRepository } from "../../../user/domain/repositories/UserRepository.ts";
 import { UserFinder } from "../../../user/domain/services/UserFinder.ts";
+import { UserId } from "../../../user/domain/value-objects/UserId.ts";
 import type { CommentRepository } from "../../domain/repositories/CommentRepository.ts";
 import { CommentFinder } from "../../domain/services/CommentFinder.ts";
 import { CommentId } from "../../domain/value-objects/CommentId.ts";
@@ -13,6 +15,7 @@ export class DeleteCommentById {
   private postFinder: PostFinder;
   private userRepository: UserRepository;
   private postRepository: PostRepository;
+  private resourceAccessChecker: ResourceAccessChecker;
   constructor(
     commentRepository: CommentRepository,
     userRepository: UserRepository,
@@ -24,15 +27,23 @@ export class DeleteCommentById {
     this.postFinder = new PostFinder(postRepository);
     this.userRepository = userRepository;
     this.postRepository = postRepository;
+    this.resourceAccessChecker = new ResourceAccessChecker();
   }
 
-  async run(id: string) {
+  async run(id: string, actorId: string) {
     const commentId = new CommentId(id);
     // Check comment existance
     const comment = await this.commentFinder.findById(commentId);
 
+    // Check permissions
+    await this.resourceAccessChecker.check(
+      UserId.fromPrimitives(actorId),
+      comment.userId
+    );
+
     const user = await this.userFinder.findById(comment.userId);
     const post = await this.postFinder.findById(comment.postId);
+
     // Trigger user and post updates
     user.deleteComment();
     post.deleteComment();
