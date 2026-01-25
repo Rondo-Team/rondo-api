c677694367505ac5a45335dedad60284dc8b14a4
Guillermo <alu0101505688@ull.edu.es>
2026-01-25 13:34:39 +0000

    feat: add GetProposalById endpoint and related bindings

---
+++ src/proposal/infrastructure/controllers/GetProposalByIdEndpoint.ts
@@ -0,0 +1,35 @@
+import { describeRoute } from "hono-openapi";
+import { validator } from "hono-openapi/zod";
+import { config } from "../../../config/infrastructure/config.ts";
+import { ApiTag } from "../../../shared/controllers/infrastructure/schemas/ApiTag.ts";
+import type { Endpoint } from "../../../shared/controllers/infrastructure/types/Endpoint.ts";
+import type { GetProposalById } from "../../application/use-cases/GetProposalById.ts";
+import { ProposalIdParamsDTO } from "./dtos/ProposalIdParamsDTO.ts";
+
+export function GetProposalByIdEndpoint(
+  getProposalById: GetProposalById,
+): Endpoint {
+  return {
+    method: "get",
+    path: `${config.app.baseUrl}/proposal/:id`,
+    secured: true,
+    handlers: [
+      describeRoute({
+        summary: "Gets a proposal information",
+        description:
+          "Allows to get a proposal information by its id. The user provides an unique id.",
+        responses: {
+          200: { description: "Proposal found" },
+        },
+        tags: [ApiTag.PROPOSAL],
+      }),
+      validator("param", ProposalIdParamsDTO),
+      async (c) => {
+        const { id } = c.req.valid("param");
+        const proposal = await getProposalById.run(id);
+        c.status(200);
+        return c.json(proposal.toPrimitives());
+      },
+    ],
+  };
+}
