051e63b5ea0a31c40bc72218fbacb1427b2cba96
Guillermo <alu0101505688@ull.edu.es>
2026-04-26 13:07:49 +0100

    feat: update GetLikeByUserAndPost use case and related endpoint tests

--- src/test/post/post-favourite/application/UserHasLikedPost.test.ts
+++ src/test/post/post-favourite/application/UserHasLikedPost.test.ts
@@ -1,19 +1,28 @@
 import { beforeEach, describe, expect, it, vi } from "vitest";
 import { PostNotFoundByIdError } from "../../../../post/domain/errors/PostNotFoundByIdError.ts";
-import { UserHasLikedPost } from "../../../../post/post-favourite/application/use-cases/UserHasLikedPost.ts";
-import { UnauthorizedUserActionError } from "../../../../shared/domain/errors/UnauthorizedUserActionError.ts";
+import { PostId } from "../../../../post/domain/value-objects/PostId.ts";
+import { GetLikeByUserAndPost } from "../../../../post/post-favourite/application/use-cases/GetLikeByUserAndPost.ts";
+import { LikeWithUserAndPostNotFoundError } from "../../../../post/post-favourite/domain/errors/LikeWithUserAndPostNotFoundError.ts";
+import { ONE_STEP_POST_FAVOURITE } from "../../../../shared/utils/domain/fixtures/postFavourites.ts";
 import { ONE_STEP_POST } from "../../../../shared/utils/domain/fixtures/posts.ts";
-import {
-  MANOLO_LOPEZ,
-  PEDRO_MARTINEZ,
-} from "../../../../shared/utils/domain/fixtures/users.ts";
+import { MANOLO_LOPEZ } from "../../../../shared/utils/domain/fixtures/users.ts";
 import { UserNotFoundByIdError } from "../../../../user/domain/errors/UserNotFoundByIdError.ts";
+import { UserId } from "../../../../user/domain/value-objects/UserId.ts";
 
-describe("Checks if a given user has liked a post", () => {
+describe("Get like by user and post use case tests", () => {
+  const mockFavourite = {
+    ...ONE_STEP_POST_FAVOURITE,
+    postId: new PostId(ONE_STEP_POST_FAVOURITE.postId),
+    userId: new UserId(ONE_STEP_POST_FAVOURITE.userId),
+    toPrimitives: vi.fn().mockReturnValue(ONE_STEP_POST_FAVOURITE),
+  };
+
   beforeEach(() => {
     userRepo.getOneById = vi.fn().mockResolvedValue(MANOLO_LOPEZ);
     postRepo.getOneById = vi.fn().mockResolvedValue(ONE_STEP_POST);
-    postFavouriteRepo.existsWithUserAndPostId = vi.fn().mockResolvedValue(true);
+    postFavouriteRepo.getByUserAndPostId = vi
+      .fn()
+      .mockResolvedValue(mockFavourite);
   });
 
   const userRepo = {
@@ -41,6 +50,7 @@ describe("Checks if a given user has liked a post", () => {
   const postFavouriteRepo = {
     create: vi.fn(),
     getOneById: vi.fn(),
+    getByUserAndPostId: vi.fn(),
     existsWithId: vi.fn(),
     existsWithUserAndPostId: vi.fn(),
     getAllByPostId: vi.fn(),
@@ -48,57 +58,46 @@ describe("Checks if a given user has liked a post", () => {
     deleteById: vi.fn(),
   };
 
-  const userHasLikedPost = new UserHasLikedPost(
+  const getLikeByUserAndPost = new GetLikeByUserAndPost(
     postFavouriteRepo,
     userRepo,
     postRepo,
   );
 
-  it("Should check if a post was liked succesfully", async () => {
-    const isLiked = await userHasLikedPost.run(
+  it("Should get the like for a user and a post succesfully", async () => {
+    const favourite = await getLikeByUserAndPost.run(
       ONE_STEP_POST.id,
-      MANOLO_LOPEZ.id,
       MANOLO_LOPEZ.id,
     );
 
-    expect(postFavouriteRepo.existsWithUserAndPostId).toBeCalledTimes(1);
-    expect(isLiked).toBe(true);
+    expect(postFavouriteRepo.getByUserAndPostId).toBeCalledTimes(1);
+    expect(favourite).toBe(mockFavourite);
   });
 
-  it("should not check if post was liked if user does not exists", async () => {
+  it("should not get the like if user does not exists", async () => {
     userRepo.getOneById = vi.fn().mockResolvedValue(null);
 
     await expect(
       async () =>
-        await userHasLikedPost.run(
-          ONE_STEP_POST.id,
-          MANOLO_LOPEZ.id,
-          MANOLO_LOPEZ.id,
-        ),
+        await getLikeByUserAndPost.run(ONE_STEP_POST.id, MANOLO_LOPEZ.id),
     ).rejects.toThrow(UserNotFoundByIdError);
   });
 
-  it("should not check if post was liked if post does not exists", async () => {
+  it("should not get the like if post does not exists", async () => {
     postRepo.getOneById = vi.fn().mockResolvedValue(null);
 
     await expect(
       async () =>
-        await userHasLikedPost.run(
-          ONE_STEP_POST.id,
-          MANOLO_LOPEZ.id,
-          MANOLO_LOPEZ.id,
-        ),
+        await getLikeByUserAndPost.run(ONE_STEP_POST.id, MANOLO_LOPEZ.id),
     ).rejects.toThrow(PostNotFoundByIdError);
   });
 
-  it("should not check if post was liked if actor is not user", async () => {
+  it("should not get the like if user has not liked the post", async () => {
+    postFavouriteRepo.getByUserAndPostId = vi.fn().mockResolvedValue(undefined);
+
     await expect(
       async () =>
-        await userHasLikedPost.run(
-          ONE_STEP_POST.id,
-          MANOLO_LOPEZ.id,
-          PEDRO_MARTINEZ.id,
-        ),
-    ).rejects.toThrow(UnauthorizedUserActionError);
+        await getLikeByUserAndPost.run(ONE_STEP_POST.id, MANOLO_LOPEZ.id),
+    ).rejects.toThrow(LikeWithUserAndPostNotFoundError);
   });
 });
