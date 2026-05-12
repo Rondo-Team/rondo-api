ddc2ca003935ab687f75d5e475249f1fffda55b2
Guillermo <alu0101505688@ull.edu.es>
2026-05-12 11:58:11 +0100

    fix: refactor GetAllCommentFavouritesByUserIdAndPostId endpoint to retrieve userId from authentication and update related tests

--- src/comment/comment-favourite/infrastructure/controllers/dtos/GetAllCommentFavouritesByUserIdAndPostIdQueryParamsDTO.ts
+++ src/comment/comment-favourite/infrastructure/controllers/dtos/GetAllCommentFavouritesByUserIdAndPostIdQueryParamsDTO.ts
@@ -1,14 +1,12 @@
 import z from "zod";
 import { extendZodWithOpenApi } from "zod-openapi";
 import { ONE_STEP_POST } from "../../../../../shared/utils/domain/fixtures/posts.ts";
-import { MANOLO_LOPEZ } from "../../../../../shared/utils/domain/fixtures/users.ts";
 
 extendZodWithOpenApi(z);
 
 export const GetAllCommentFavouritesByUserIdAndPostIdQueryParamsDTO = z
   .object({
     postId: z.string().uuid().openapi({ example: ONE_STEP_POST.id }),
-    userId: z.string().uuid().openapi({ example: MANOLO_LOPEZ.id }),
   })
   .openapi({
     description: "Query params for searching liked comments",
